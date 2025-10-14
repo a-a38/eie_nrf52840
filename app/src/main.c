e6124798f1c8b356e7059fa2802b52cd5327deb7
Anaya <anayadil.38@gmail.com>
2025-10-13 21:34:48 -0600

    Make all LEDs blink

--- app/src/main.c
+++ app/src/main.c
@@ -1,14 +1,35 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
+
+static const struct gpio_dt_spec leds[] = {
+	GPIO_DT_SPEC_GET(LED0_NODE, gpios),
+	GPIO_DT_SPEC_GET(LED1_NODE, gpios),
+	GPIO_DT_SPEC_GET(LED2_NODE, gpios),
+	GPIO_DT_SPEC_GET(LED3_NODE, gpios),
+};
+
 int main(void) {
-
-	while (1) {
-
+	int ret;
+	for (int i=0; i<4; i++)  {
+		if (!gpio_is_ready_dt(&leds[i])) {
+			return -1;
+		}
+	gpio_pin_configure_dt(&leds[i], GPIO_OUTPUT_ACTIVE);
+		if (ret < 0) {
+			return ret;
+		}
 	}
 
-	return 0;
-
+	while (1) {
+		for (int i = 0; i <4; i++) {
+			gpio_pin_toggle_dt(&leds[i]);
+		}
+		k_msleep(1000);
+	}
 }
-
 
