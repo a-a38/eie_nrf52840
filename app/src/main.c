46e720027692392abc64bb62d9f2e6a0d486c73b
Anaya <anayadil.38@gmail.com>
2025-11-16 21:00:49 -0700

    Sve lesson 6 content before starting project

--- app/src/main.c
+++ app/src/main.c
@@ -2,44 +2,37 @@
  * main.c
  */
 
-#include <stdio.h>
+//#include <stdio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+//#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
+
+#include "LED.h"
+#include "BTN.h"
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#define SLEEP_TIME_MS 100  
 
 int main(void)
 {
-    int ret;
-    bool led_state = true;
-
-    if (!gpio_is_ready_dt(&led)) {
+    if (0 > LED_init()){
+        return 0;
+    }
+    if (0 > BTN_init()){
         return 0;
     }
 
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        return 0;
-    }
+    uint8_t current_duty_cycle = 0;
 
-    while (1) {
-        ret = gpio_pin_toggle_dt(&led);
-        if (ret < 0) {
-            return 0;
+    LED_pwm(LED0, current_duty_cycle);
+
+    while(1){
+        if (BTN_check_clear_pressed(BTN0)){
+            current_duty_cycle = (current_duty_cycle >= 100) ? 0: (current_duty_cycle + 10);
+            printk("Setting LED0 to %d%% brightness.\n", current_duty_cycle);
+            LED_pwm(LED0, current_duty_cycle);
         }
-
-        led_state = !led_state;
-        printf("LED state: %s\n", led_state ? "ON" : "OFF");
         k_msleep(SLEEP_TIME_MS);
     }
     return 0;
